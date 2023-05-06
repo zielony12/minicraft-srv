2933b65532ce0c4e44deb7fd08017225b07228bc
Karol <xdzielony@gmail.com>
2023-05-06 15:51:49 +0200

    added Level.setTile

--- src/io/github/zielony12/minicraftsrv/ServerThread.java
+++ src/io/github/zielony12/minicraftsrv/ServerThread.java
@@ -8,15 +8,11 @@ public class ServerThread extends Thread {
 	protected DatagramSocket socket;
 	private boolean running;
 	private int port;
-	private static final String[] responses = { "Hello!", "I'm an UDP server!", "Wassup?" };
-	private int iterator;
 
 	public ServerThread(int port, String name) throws IOException {
 		super(name);
 		socket = new DatagramSocket(port);
 		this.port = port;
-
-		iterator = 0;
 	}
 
 	public void run() {
@@ -30,45 +26,30 @@ public class ServerThread extends Thread {
 				int port = packet.getPort();
 				switch(buffer[0]) {
 					case 0:
-						System.out.println("Client asks for map");
+						System.out.println("Client asks for tile array");
 						buffer = Server.level.tiles;
-						//TODO: load map to buffer
 						break;
 					case 1:
-						System.out.println("Client asks for entities");
-						//TODO: load entities to buffer
+						System.out.println("Client asks for data array");
+						buffer = Server.level.data;
 						break;
 					case 2:
 						int x = buffer[1];
 						int y = buffer[2];
-						int id = buffer[3];
-						switch(id) {
-							case 3: // flower (breaks instantly)
-								System.out.println("Client breaks a tile on " + x + ", " + y + " of id " + id);
-								
-								//TODO: move tiles operations to Level class
-								Server.level.tiles[x + y * Server.level.w] = 0; // to grass
-								break;
-						}
-						//TODO: receive tile info
+						byte id = buffer[3];
+						byte dataVal = buffer[4];
+						System.out.println("Client updates a tile on " + x + ", " + y + " from id " + (int) Server.level.tiles[(int) x + (int) y * Server.level.w] + " to id " + (int) id + " and data " + (int) dataVal);
+						Server.level.setTile(x, y, id, dataVal);
 						break;
 				}
 
 				packet = new DatagramPacket(buffer, buffer.length, address, port);
 				socket.send(packet);
-				setNext();
 			} catch(IOException e) {
 				e.printStackTrace();
 				running = false;
 			}
 		}
 		socket.close();
-	}
-	
-	public void setNext() {
-		if(iterator >= responses.length-1)
-			iterator = 0;
-		else
-			iterator ++;
 	}
 }
